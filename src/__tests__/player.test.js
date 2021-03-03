b8a947850a48629d5ee6ab164767b9beb758e80b
Darek Kilian <emis@MacBook-Pro.local>
2021-03-03 19:48:19 +0100

    Added GH-Pages

---
+++ src/__tests__/player.test.js
@@ -0,0 +1,7 @@
+// import React from "react";
+// import { render, fireEvent } from "@testing-library/react";
+// import { Player } from "../components";
+
+// describe("<Player />", () => {
+//   if (("renders the <Player /> with a bunny video", () => {}));
+// });
